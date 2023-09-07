0e2d05fdb51970f9db954780945e2c34dcaf4d8e
fahrul <fahruluyah12@gmail.com>
2023-09-07 12:38:53 +0700

    WEEK4

---
+++ 1.js
@@ -0,0 +1,12 @@
+console.log("hello")
+let nilai1 = 2
+let nilai2 = 7
+let total = nilai1 + nilai2
+console.log(total)
+nilai1 = "A"
+nilai2 = 7
+nilai3 = true
+total2 = nilai1 + nilai2 + nilai3
+console.log(total2)
+
+let grade = ipk>3.5 ? "cumlaude" : ipk=>
